27aadf6c2d217aaa1b8b82896d90c178aa8b6632
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2026-01-22 09:24:30 +0530

    fix: separate function in modules

--- src/inject-navigation-buttons.ts
+++ src/inject-navigation-buttons.ts
@@ -1,26 +1,6 @@
-const scrollObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const articles = Array.from(document.querySelectorAll("article"));
-        const index = articles.indexOf(entry.target as HTMLElement);
+import { scrollObserver } from "./scroll-observer";
 
-        document
-          .querySelectorAll(".nav-btn")
-          .forEach((btn) => btn.classList.remove("active-nav"));
-
-        const activeBtn = document.querySelectorAll(".nav-btn")[index];
-        if (activeBtn) activeBtn.classList.add("active-nav");
-      }
-    });
-  },
-  {
-    threshold: 0.1,
-    rootMargin: "-20% 0px -20% 0px",
-  },
-);
-
-function injectNavigationButtons() {
+export function injectNavigationButtons() {
   const chatArray = document.querySelectorAll("article");
   const threadBottomContainer = document.querySelector(
     "#thread-bottom-container",
@@ -62,14 +42,3 @@ function injectNavigationButtons() {
     btnContainer.appendChild(btn);
   });
 }
-
-injectNavigationButtons();
-
-const observer = new MutationObserver(() => {
-  injectNavigationButtons();
-});
-
-observer.observe(document.querySelector("main")!, {
-  childList: true,
-  subtree: true,
-});
